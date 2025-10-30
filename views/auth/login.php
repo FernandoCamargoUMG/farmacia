8e6bd1fe4e375ef49f7a73b1b876b3ad5aa586cc
KCHAVEZ99 <110781229+KCHAVEZ99@users.noreply.github.com>
2025-10-29 21:43:21 -0600

    cambios de titulos

--- views/auth/login.php
+++ views/auth/login.php
@@ -5,7 +5,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Login - Farmacia</title>
+    <title>Login - Ferreteria</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
--- views/layout/header.php
+++ views/layout/header.php
@@ -24,7 +24,7 @@ if (isset($_SESSION['sucursal_id'])) {
 
 <head>
     <meta charset="UTF-8">
-    <title>Farmacia</title>
+    <title>Ferreteria</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap 5 + Íconos -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
